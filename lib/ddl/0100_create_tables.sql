408ad45cf39e130722ae46aca117855439382369
Paul Water <p.r.water@setbased.nl>
2017-04-20 15:10:53 +0200

    Initial commit.

---
+++ lib/ddl/0100_create_tables.sql
@@ -0,0 +1,131 @@
+/*================================================================================*/
+/* DDL SCRIPT                                                                     */
+/*================================================================================*/
+/*  Title    : ABC Request Logger Light                                           */
+/*  FileName : abc-request-logger-light.ecm                                       */
+/*  Platform : MySQL 5.6                                                          */
+/*  Version  : 1.0.0                                                              */
+/*  Date     : donderdag 20 april 2017                                            */
+/*================================================================================*/
+/*================================================================================*/
+/* CREATE TABLES                                                                  */
+/*================================================================================*/
+
+CREATE TABLE LOG_REQUEST (
+  rql_id INTEGER AUTO_INCREMENT NOT NULL,
+  cmp_id SMALLINT NOT NULL,
+  pag_id SMALLINT,
+  ses_id INTEGER,
+  usr_id INTEGER NOT NULL,
+  rql_datetime DATETIME NOT NULL,
+  rql_request VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  rql_method VARCHAR(8) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  rql_referrer VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  rql_ip INT,
+  rql_host_name VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  rql_accept_language VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  rql_user_agent VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci,
+  rql_number_of_queries INT,
+  rql_time FLOAT,
+  rql_size INT,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (rql_id)
+);
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_datetime
+The date and time of the HTTP request
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_request
+The requested URL
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_method
+The request method used to access the page
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_referrer
+The URL of the page which referred the user agent to the page
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_ip
+The IP address from which the page is requested
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_accept_language
+The accepted languages by the user agent
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_user_agent
+The user agent use the request the page
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_number_of_queries
+The number of queries executed for processing the page request
+*/
+
+/*
+COMMENT ON COLUMN LOG_REQUEST.rql_time
+The (real)time required for processing the page request
+*/
+
+CREATE TABLE LOG_REQUEST_COOKIE (
+  rql_id INTEGER NOT NULL,
+  rcl_variable VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  rcl_value MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci
+);
+
+CREATE TABLE LOG_REQUEST_POST (
+  rql_id INTEGER NOT NULL,
+  rlp_variable VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  rlp_value MEDIUMTEXT CHARACTER SET utf8 COLLATE utf8_general_ci
+);
+
+CREATE TABLE LOG_REQUEST_QUERY (
+  rqq_id INTEGER AUTO_INCREMENT NOT NULL,
+  rql_id INTEGER NOT NULL,
+  rqq_query MEDIUMBLOB NOT NULL,
+  rqq_time FLOAT NOT NULL,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (rqq_id)
+);
+
+/*================================================================================*/
+/* CREATE INDEXES                                                                 */
+/*================================================================================*/
+
+CREATE INDEX IX_FK_LOG_REQUEST ON LOG_REQUEST (pag_id);
+
+CREATE INDEX IX_FK_LOG_REQUEST1 ON LOG_REQUEST (usr_id);
+
+CREATE INDEX IX_FK_LOG_REQUEST2 ON LOG_REQUEST (cmp_id);
+
+CREATE INDEX IX_FK_LOG_REQUEST3 ON LOG_REQUEST (ses_id);
+
+CREATE INDEX IX_FK_LOG_REQUEST_COOKIE ON LOG_REQUEST_COOKIE (rql_id);
+
+CREATE INDEX rql_id ON LOG_REQUEST_POST (rql_id);
+
+CREATE INDEX IX_FK_LOG_REQUEST_QUERY ON LOG_REQUEST_QUERY (rql_id);
+
+/*================================================================================*/
+/* CREATE FOREIGN KEYS                                                            */
+/*================================================================================*/
+
+ALTER TABLE LOG_REQUEST_COOKIE
+  ADD CONSTRAINT FK_LOG_REQUEST_COOKIE_LOG_REQUEST
+  FOREIGN KEY (rql_id) REFERENCES LOG_REQUEST (rql_id);
+
+ALTER TABLE LOG_REQUEST_POST
+  ADD CONSTRAINT FK_LOG_REQUEST_POST_LOG_REQUEST
+  FOREIGN KEY (rql_id) REFERENCES LOG_REQUEST (rql_id);
+
+ALTER TABLE LOG_REQUEST_QUERY
+  ADD CONSTRAINT FK_LOG_REQUEST_QUERY_LOG_REQUEST
+  FOREIGN KEY (rql_id) REFERENCES LOG_REQUEST (rql_id);
---
+++ src/RequestLoggerLight.php
@@ -0,0 +1,170 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\RequestLogger;
+
+use SetBased\Abc\Abc;
+use SetBased\Abc\C;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * A HTTP page request logger for light and development websites.
+ */
+class RequestLoggerLight implements RequestLogger
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * If true the HTTP page request must be logged.
+   *
+   * @var bool
+   */
+  public $logRequest;
+
+  /**
+   * If true the HTTP page request details (i.e. cookies, post variables and queries) must be logged.
+   *
+   * @var bool
+   */
+  public $logRequestDetails;
+
+  /**
+   * The ID of the logged page request.
+   *
+   * @var int|null
+   */
+  public $rqlId;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   */
+  public function __construct()
+  {
+    $this->logRequest        = true;
+    $this->logRequestDetails = false;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Logs the HTTP page request.
+   *
+   * @param int $status The HTTP status code.
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function logRequest($status)
+  {
+    if ($this->logRequest)
+    {
+      $abc = Abc::getInstance();
+
+      $this->rqlId = Abc::$DL->abcRequestLogInsertRequest($abc->getSesId(),
+                                                          $abc->getCmpId(),
+                                                          $abc->getUsrId(),
+                                                          $abc->getPagId(),
+                                                          mb_substr($_SERVER['REQUEST_URI'] ?? '', 0, C::LEN_RQL_REQUEST),
+                                                          mb_substr($_SERVER['REQUEST_METHOD'] ?? '', 0, C::LEN_RQL_METHOD),
+                                                          mb_substr($_SERVER['HTTP_REFERER'] ?? '', 0, C::LEN_RQL_REFERRER),
+                                                          $_SERVER['REMOTE_ADDR'] ?? null,
+                                                          mb_substr($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '', 0, C::LEN_RQL_LANGUAGE),
+                                                          mb_substr($_SERVER['HTTP_USER_AGENT'] ?? '', 0, C::LEN_RQL_USER_AGENT),
+                                                          null,
+                                                          microtime(true) - Abc::$time0,
+                                                          null);
+
+      if ($this->logRequestDetails)
+      {
+        $oldLogQueries       = Abc::$DL->logQueries;
+        Abc::$DL->logQueries = false;
+
+        $this->requestLogQuery();
+        $this->requestLogPost($_POST);
+        $this->requestLogCookie($_COOKIE);
+
+        Abc::$DL->logQueries = $oldLogQueries;
+      }
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Logs the (by the user agent) sent cookies into the database.
+   *
+   * Usage on this method on production environments is not recommended.
+   *
+   * @param array       $cookies    must be $_COOKIES
+   * @param string|null $parentName must not be used, intended for use by recursive calls only.
+   */
+  private function requestLogCookie($cookies, $parentName = null)
+  {
+    if (is_array($cookies))
+    {
+      foreach ($cookies as $name => $value)
+      {
+        $fullName = ($parentName===null) ? $name : $parentName.'['.$name.']';
+
+        if (is_array($value))
+        {
+          $this->requestLogCookie($value, $fullName);
+        }
+        else
+        {
+          Abc::$DL->abcRequestLogInsertCookie($this->rqlId, $fullName, $value);
+        }
+      }
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Logs the post variables into the database.
+   *
+   * Usage on this method on production environments is not recommended.
+   *
+   * @param array       $post       Must be $_POST (except for recursive calls).
+   * @param string|null $parentName Must not be used (except for recursive calls).
+   */
+  private function requestLogPost($post, $parentName = null)
+  {
+    if (is_array($post))
+    {
+      foreach ($post as $name => $value)
+      {
+        $fullName = ($parentName===null) ? $name : $parentName.'['.$name.']';
+
+        if (is_array($value))
+        {
+          $this->requestLogPost($value, $fullName);
+        }
+        else
+        {
+          // Don't log passwords.
+          if (strpos($name, 'password')!==false)
+          {
+            $value = str_repeat('*', mb_strlen($name));
+          }
+
+          Abc::$DL->abcRequestLogInsertPost($this->rqlId, $fullName, $value);
+        }
+      }
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Logs the executed SQL queries into the database.
+   */
+  private function requestLogQuery()
+  {
+    $queries = Abc::$DL->getQueryLog();
+
+    foreach ($queries as $query)
+    {
+      Abc::$DL->abcRequestLogInsertQuery($this->rqlId, $query['query'], $query['time']);
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
